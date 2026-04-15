0f86b93e474149c63f6d5294aa6961b091f9985f
Mimocake <rchekr@yandex.ru>
2026-04-15 08:55:15 +0300

    updated to lab03 cmake config

--- solver_lib/solver.cpp
+++ solver_lib/solver.cpp
@@ -1,21 +1,17 @@
 #include "solver.h"
 
 #include <stdexcept>
-#ifdef _WIN32
-	#include <cmath>
-#else
-	#include <math.h>
-#endif
+#include <math.h>
 
 void solve(float a, float b, float c, float& x1, float& x2)
 {
-    float d = (b * b) - (4.0f * a * c);
+    float d = (b * b) - (4 * a * c);
 
-    if (d < 0.0f)
+    if (d < 0)
     {
         throw std::logic_error{"error: discriminant < 0"};
     }
 
-    x1 = (-b - std::sqrt(d)) / (2.0f * a);
-    x2 = (-b + std::sqrt(d)) / (2.0f * a);
+    x1 = (-b - std::sqrt(d)) / (2 * a);
+    x2 = (-b + std::sqrt(d)) / (2 * a);
 }
